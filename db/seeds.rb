4e23260012180b095003b63b0bd0cc2b41705e63
Moritz Metelmann <moritz@metelmann.biz>
2022-05-18 14:23:33 +0200

    Yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+categories = %w(chinese italian japanese french belgian)
+
+10.times do
+  @restaurant = Restaurant.new(name: Faker::Restaurant.name, address: Faker::Address.full_address, phone_number: Faker::PhoneNumber.phone_number_with_country_code, category: categories[rand(categories.length)])
+  @restaurant.save
+end
+
+50.times do
+  @review = Review.new(rating: rand(1..5), content: Faker::Food.description, restaurant_id: rand(1..10))
+  @review.save
+end
